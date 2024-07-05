64ece37f4930a1b28894064a2303e76630c51aa5
PC <PC@MYCOMPUTER>
2024-07-05 08:42:44 +0700

    Add Controller Modified Product State

--- FinalProject-TayViet-Accessory-Store-Management.Server/Controllers/ProductController.cs
+++ FinalProject-TayViet-Accessory-Store-Management.Server/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using FinalProject_TayViet_Accessory_Store_Management.Server.Models;
+﻿using FinalProject_TayViet_Accessory_Store_Management.Models.ExceptionModels;
+using FinalProject_TayViet_Accessory_Store_Management.Server.Models;
+using FinalProject_TayViet_Accessory_Store_Management.Server.Utility.ValidateState;
 using FinalProject_TayViet_Accessory_Store_Management.Utility.DatabaseUtility;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,6 +10,34 @@ namespace FinalProject_TayViet_Accessory_Store_Management.Server.Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerTemplate<Product>
     {
-        public ProductController(ProductDatabaseService productDatabaseService) : base(databaseServices: productDatabaseService) { }
+        public ProductController(ProductDatabaseService productDatabaseService) : base(databaseServices: productDatabaseService) => _databaseServices = productDatabaseService;
+        private new readonly ProductDatabaseService _databaseServices;
+
+        [HttpPut("SetState/{id}, {subProductName}, {newState}")]
+        public async Task<IActionResult> UpdateProductState(string id, string subProductName, string newState)
+        {
+            if (ProductValidateState.PRODUCT_STATE_DICTIONARY.ContainsKey(newState) == false)
+            {
+                return BadRequest("Invalid state");
+            }
+
+            Product product;
+            try
+            {
+                product = await _databaseServices.ReadAsync("id", id);
+            }
+            catch (FormatException) { return BadRequest("Invalid Id"); }
+            catch (NotFoundException) { return NotFound("Item Not Found Or Deleted"); }
+            catch (Exception) { throw new UnknownException(); }
+
+            SubProduct? subProduct = product.subProductList.Find(x => x.name == subProductName);
+            if (subProduct == null)
+            {
+                return NotFound("Item Not Found Or Deleted");
+            }
+
+            await _databaseServices.ChangeSubProductState(id, subProductName, newState);
+            return Ok();
+        }
     }
 }
--- FinalProject-TayViet-Accessory-Store-Management.Server/Utility/DatabaseUtility/ProductDatabaseService.cs
+++ FinalProject-TayViet-Accessory-Store-Management.Server/Utility/DatabaseUtility/ProductDatabaseService.cs
@@ -1,13 +1,24 @@
-﻿using FinalProject_TayViet_Accessory_Store_Management.Models;
-using FinalProject_TayViet_Accessory_Store_Management.Server.Models;
+﻿using FinalProject_TayViet_Accessory_Store_Management.Server.Models;
 using Microsoft.Extensions.Options;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace FinalProject_TayViet_Accessory_Store_Management.Utility.DatabaseUtility
 {
-    // This class is used to set product collection and model for this databaseServices. To use method inside this class, check the DatabaseServices.cs
     public class ProductDatabaseService : DatabaseServices<Product>
     {
         public ProductDatabaseService(IOptions<DBSettings> dbSettings, int index_collection = 4) : base(dbSettings, index_collection) { }
+
+        public async Task ChangeSubProductState(string id, string subProductName, string state)
+        {
+            var filter = Builders<Product>.Filter.Eq("id", id);
+            var update = Builders<Product>.Update.Set("subProductList.$[s].state", state);
+            var arrayFilters = new List<ArrayFilterDefinition>
+            {
+                new BsonDocumentArrayFilterDefinition<Product>(new BsonDocument("s.name", subProductName))
+            };
+            var options = new UpdateOptions { ArrayFilters = arrayFilters };
+            await _collection.UpdateOneAsync(filter, update, options);
+        }
     }
 }
