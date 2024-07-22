1ff50ba59fe726a3856c12a08ae39815a2779622
mkhain.000 <duongdhbh00631@fpt.edu.vn>
2024-07-22 12:22:25 +0700

    login and table css

--- finalproject-tayviet-accessory-store-management.admin/src/app/app.module.ts
+++ finalproject-tayviet-accessory-store-management.admin/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MessageBoxComponent } from './message-box/message-box.component';
@@ -11,6 +11,7 @@ import { AlertBoxComponent } from './alert-box/alert-box.component';
 import { TableListComponent } from './table-list/table-list.component';
 import { UserInfoBoxComponent } from './user-info-box/user-info-box.component';
 import { DetailProductComponent } from './detail-product/detail-product.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 @NgModule({
   declarations: [
@@ -19,11 +20,12 @@ import { DetailProductComponent } from './detail-product/detail-product.componen
     MessageBoxComponent,
     TableListComponent,
     UserInfoBoxComponent,
-    DetailProductComponent
+    DetailProductComponent,
+    AdminLoginComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
-    AppRoutingModule, BrowserAnimationsModule
+    AppRoutingModule, BrowserAnimationsModule, FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
